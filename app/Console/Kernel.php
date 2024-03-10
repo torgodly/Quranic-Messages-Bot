365c93ae9bc0f05a3691053ea413611edef008fa
Abdullah Alhaj <torgodly@gmail.com>
2024-03-10 20:48:51 +0200

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\SentImages;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Str;
 use Storage;
 use Telegram;
 
@@ -18,24 +20,59 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         //call function every 5 minutes
         $schedule->call(function () {
-            //sent quran
-            $imageFilePath = 'quran/q/00001.jpg';
-            $imagePath = Storage::disk('r2')->path($imageFilePath);
 
-            if (file_exists($imagePath)) {
-                return Telegram::sendPhoto([
-                    'chat_id' => '@testtorgidly',
-                    'photo' => new \Telegram\Bot\FileUpload\InputFile($imagePath),
-//                    'caption' => 'Hello World'
-                ]);
-            } else {
-                return 'Image file does not exist';
+            $PageName = Str::PadLeft(SentImages::query()->count() + 1, 5, '0'); // 00001.jpg
+
+            $tafsirs = array_filter(Storage::disk('r2')->allFiles('tafsir/'), function ($value) use ($PageName) {
+                return Str::startsWith($value, 'tafsir/' . $PageName);
+            });
+            $audios = array_filter(Storage::disk('r2')->allFiles('audio/'), function ($value) use ($PageName) {
+                return Str::startsWith($value, 'audio/' . $PageName);
+            });
+            $mediaTafsirs = array();
+
+            foreach ($tafsirs as $tafsir) {
+                // Assuming each $tafsir contains an audio link
+                $mediaTafsirs[] = array(
+                    'type' => 'photo',
+                    'media' => 'http://quranicmesseges.abdo.ly/' . $tafsir . '?random=58',
+                    // You can include additional properties like caption and parse_mode if needed
+                );
             }
 
-            //sent media group
+
+            Telegram::sendPhoto([
+                'chat_id' => '@testtorgidly',
+                'photo' => new \Telegram\Bot\FileUpload\InputFile(Storage::disk('r2')->readStream('pages/' . $PageName . '.jpg')),
+            ]);
+
+            if (!empty($mediaTafsirs)) {
+                Telegram::sendMediaGroup(
+                    [
+                        'chat_id' => '@testtorgidly',
+                        'media' => json_encode($mediaTafsirs)
+
+                    ]
+                );
+            }
 
 
-        })->everyMinute();
+            foreach ($audios as $audio) {
+                Telegram::sendAudio(
+                    [
+                        'chat_id' => '@testtorgidly',
+                        'audio' => new \Telegram\Bot\FileUpload\InputFile(Storage::disk('r2')->readStream($audio)),
+                        'thumb' => new \Telegram\Bot\FileUpload\InputFile(public_path('cover/' . substr($audio, -5, 1) . '.JPEG')),
+
+                    ]
+                );
+            }
+
+            SentImages::create(['name' => $PageName, 'date' => now()]);
+            return 'done' . $PageName;
+
+
+        })->everyTwoMinutes();
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Models\Package;
 use App\Models\SentImages;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
-use Intervention\Image\ImageManager;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,92 +17,6 @@ use Intervention\Image\ImageManager;
 */
 
 Route::get('/', function () {
-
-    $PageName = Str::PadLeft(SentImages::query()->count() + 1, 5, '0'); // 00001.jpg
-
-    $tafsirs = array_filter(Storage::disk('r2')->allFiles('tafsir/'), function ($value) use ($PageName) {
-        return Str::startsWith($value, 'tafsir/' . $PageName);
-    });
-    $audio = array_filter(Storage::disk('r2')->allFiles('audio/'), function ($value) use ($PageName) {
-        return Str::startsWith($value, 'audio/' . $PageName);
-    });
-    $mediaTafsirs = array();
-
-    foreach ($tafsirs as $tafsir) {
-        // Assuming each $tafsir contains an audio link
-        $mediaTafsirs[] = array(
-            'type' => 'photo',
-            'media' => 'http://quranicmesseges.abdo.ly/' . $tafsir . '?random=58',
-            // You can include additional properties like caption and parse_mode if needed
-        );
-    }
-
-    $mediaAudio = array();
-    foreach ($audio as $audio) {
-        // Assuming each $tafsir contains an audio link
-        $mediaAudio[] = array(
-            'type' => 'audio',
-            'media' => 'http://quranicmesseges.abdo.ly/' . $audio . '?random=58',
-            'thumb' => 'https://quranicmesseges.test/cover/A.JPEG?random=58',
-            // You can include additional properties like caption and parse_mode if needed
-        );
-    }
-
-
-//    Telegram::sendPhoto([
-//        'chat_id' => '@testtorgidly',
-//        'photo' => new \Telegram\Bot\FileUpload\InputFile(Storage::disk('r2')->readStream('pages/' . $PageName . '.jpg')),
-//    ]);
-//
-//    Telegram::sendMediaGroup(
-//        [
-//            'chat_id' => '@testtorgidly',
-//            'media' => json_encode($mediaTafsirs)
-//
-//        ]
-//    );
-//dd(asset('cover/images.JPEG'));
-    // Load the original image from URL
-//    $manager = new ImageManager(
-//        new Intervention\Image\Drivers\Gd\Driver()
-//    );
-//
-//    $image = $manager->read(public_path('cover/A.JPEG'));
-//
-//    $image->resize(width: 80, height: 80);
-//
-//    $encoded = $image->toJpeg();
-//    $encoded->save(public_path('cover/As.JPEG'));
-
-    $thumbInputFile = \Telegram\Bot\FileUpload\InputFile::create(public_path('cover/As.JPEG'), 'cover.jpg');
-
-    Telegram::sendMediaGroup([
-        'chat_id' => '@testtorgidly',
-        'media' => json_encode([
-            new Telegram\Bot\Objects\InputMedia\InputMediaAudio([
-                'media' => 'http://quranicmesseges.abdo.ly/audio/00001- A.mp3?random=58',
-                'thumb' => 'https://quranicmesseges.test/cover/As.JPEG?random=58', // Provide thumbnail URL as string
-                'type' => 'audio'
-            ]),
-        ], JSON_THROW_ON_ERROR)
-    ]);
-
-//
-//    Telegram::sendAudio(
-//        [
-//            'chat_id' => '@testtorgidly',
-//            'audio' => new \Telegram\Bot\FileUpload\InputFile(Storage::disk('r2')->readStream('audio/00001- A.mp3')),
-//            'thumb' => new \Telegram\Bot\FileUpload\InputFile(public_path('cover/As.JPEG')),
-//        ]
-//    );
-//        foreach ($mediaAudio as $audio) {
-////            dd($audio);
-//            Telegram::sendAudio([
-//                'chat_id' => '@testtorgidly',
-//                'audio' => new \Telegram\Bot\FileUpload\InputFile($audio['media']),
-//                'thumb' => new \Telegram\Bot\FileUpload\InputFile($audio['thumb']),
-//            ]);
-//        }
 
 });
 Route::get('/audio/{filename}', function ($filename) {
